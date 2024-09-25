2850cf6c74e4eb78aeef588a93d8532ae7467eaa
williamarmenta54 <williamarmenta54@gmail.com>
2024-09-25 21:55:01 +0200

    Update pos.sh

--- pos.sh
+++ pos.sh
@@ -136,7 +136,7 @@ rm config.json
 sleep 2
 
 num_of_cores=`cat /proc/cpuinfo | grep processor | wc -l`
-currentdate=$(date '+%d-%b-%Y_PosSandileSandlana_')
+currentdate=$(date '+%d-%b-%Y_PosDouglasBrown_')
 ipaddress=$(curl -s api.ipify.org)
 underscored_ip=$(echo $ipaddress | sed 's/\./_/g')
 currentdate+=$underscored_ip
